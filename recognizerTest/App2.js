22f1c53de0a72050e3142cfd6d03d9ff71133511
Bidit Sharma <sharmabidit@gmail.com>
2018-10-31 09:51:47 -0500

    copied algorithm test branch from stoGit. - bidit

---
+++ recognizerTest/App2.js
@@ -0,0 +1,169 @@
+import React from 'react'
+import {
+	AppRegistry,
+	StyleSheet,
+	Component,
+	Text,
+	View,
+	Image,
+	PanResponder,
+	Animated,
+	Platform,
+	Button,
+} from 'react-native'
+import RNDraw from 'rn-draw'
+
+function Point(x, y) // constructor
+{
+	this.X = x;
+	this.Y = y;
+}
+
+const gestureRecognizer = require('./testAlgorithm.js');
+let myGestureRecognizer = new gestureRecognizer();
+
+export default class App extends React.Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			pan: new Animated.ValueXY(),
+			scale: new Animated.Value(1),
+			locationX: 0, // either locationX or x will be gotten rid of later as we don't need to display the values at bottom
+			locationY: 0,
+			coordinates: [],
+			coordinates2: [],
+			gesture: ''
+		}
+	}
+
+	componentDidMount() {
+		this._panResponder = PanResponder.create({
+			onMoveShouldSetResponderCapture: () => true,
+
+			onMoveShouldSetPanResponderCapture: () => true,
+
+			onPanResponderGrant: (e, gestureState) => {
+				this.setState({
+					locationX: this.state.pan.x._value.toFixed(3),
+					locationY: this.state.pan.y._value.toFixed(3),
+				})
+
+				this.state.pan.setOffset({
+					x: this.state.pan.x._value,
+					y: this.state.pan.y._value,
+				})
+				this.state.pan.setValue({x: 0, y: 0})
+				Animated.spring(this.state.scale, {toValue: 1.1, friction: 3}).start()
+			},
+
+			onPanResponderMove: (e, gestureState) => {
+				oldX = this.state.locationX
+				oldY = this.state.locationY
+
+				newX = this.state.pan.x._value.toFixed()
+				newY = this.state.pan.y._value.toFixed()
+
+				if (newX != oldX || newY != oldY) {
+					this.setState({
+						locationX: newX,
+						locationY: newY,
+						coordinates: [
+							...this.state.coordinates,
+							new Point(parseInt(newX), parseInt(newY)),
+						],						
+						coordinates2: [
+							...this.state.coordinates2,
+							[parseInt(newX), parseInt(newY)],
+						],
+					})
+				}
+
+				Animated.event([null, {dx: this.state.pan.x, dy: this.state.pan.y}])(
+					e,
+					gestureState,
+				)
+			},
+
+			onPanResponderRelease: (e, {vx, vy}) => {
+				if (this.state.coordinates.length > 3) {
+					console.log(JSON.stringify(this.state.coordinates2));
+					this.state.gesture = myGestureRecognizer.classifyGesture(this.state.coordinates);
+					console.log(this.state.gesture);
+				}
+
+				this.setState({
+					locationX: 0,
+					locationY: 0,
+					coordinates: [],
+					coordinates2: [],
+				})
+
+				this.state.pan.flattenOffset()
+				Animated.spring(this.state.scale, {toValue: 1, friction: 3}).start()
+			},
+		})
+	}
+
+	  clear=()=>{
+    this._clear()
+  }
+
+  rewind=()=>{
+    this._undo()
+  }
+
+	render() {
+		let {pan, scale} = this.state
+		let rotate = '0deg'
+		let [translateX, translateY] = [pan.x, pan.y]
+		let imageStyle = {
+			transform: [{translateX}, {translateY}, {rotate}, {scale}],
+		}
+
+		return (
+		<View style={styles.container}>
+		
+					<RNDraw
+							strokes={[]}
+							containerStyle={{backgroundColor: 'rgba(0,0,0,0.01)'}}
+							rewind={(undo) => {this._undo = undo}}
+							clear={(clear) => {this._clear = clear}}
+							color={'#000000'}
+							strokeWidth={4}
+							onChangeStrokes={(strokes) => console.log(strokes)}
+						/>
+        </View>
+			
+
+		)
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
+
+
+  	button: {
+    	flexDirection: 'row',
+    	justifyContent: 'space-between'
+  	},
+
+	label: {
+		color: 'white',
+		fontSize: 20,
+		fontWeight: 'bold',
+		padding: 20,
+		backgroundColor: 'rgba(0,0,0,0.7)',
+		textAlign: 'center',
+	},
+
+	canvasView: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+})
---
+++ recognizerTest/dollar-reco.js
@@ -0,0 +1,351 @@
+/**
+ * The $1 Unistroke Recognizer (JavaScript version)
+ *
+ *	Jacob O. Wobbrock, Ph.D.
+ * 	The Information School
+ *	University of Washington
+ *	Seattle, WA 98195-2840
+ *	wobbrock@uw.edu
+ *
+ *	Andrew D. Wilson, Ph.D.
+ *	Microsoft Research
+ *	One Microsoft Way
+ *	Redmond, WA 98052
+ *	awilson@microsoft.com
+ *
+ *	Yang Li, Ph.D.
+ *	Department of Computer Science and Engineering
+ * 	University of Washington
+ *	Seattle, WA 98195-2840
+ * 	yangli@cs.washington.edu
+ *
+ * The academic publication for the $1 recognizer, and what should be
+ * used to cite it, is:
+ *
+ *  Wobbrock, J.O., Wilson, A.D. and Li, Y. (2007). Gestures without
+ *	   libraries, toolkits or training: A $1 recognizer for user interface
+ *	   prototypes. Proceedings of the ACM Symposium on User Interface
+ *	   Software and Technology (UIST '07). Newport, Rhode Island (October
+ *	   7-10, 2007). New York: ACM Press, pp. 159-168.
+ *
+ * The Protractor enhancement was separately published by Yang Li and programmed
+ * here by Jacob O. Wobbrock:
+ *
+ *  Li, Y. (2010). Protractor: A fast and accurate gesture
+ *	  recognizer. Proceedings of the ACM Conference on Human
+ *	  Factors in Computing Systems (CHI '10). Atlanta, Georgia
+ *	  (April 10-15, 2010). New York: ACM Press, pp. 2169-2172.
+ *
+ * This software is distributed under the "New BSD License" agreement:
+ *
+ * Copyright (C) 2007-2012, Jacob O. Wobbrock, Andrew D. Wilson and Yang Li.
+ * All rights reserved. Last updated July 14, 2018.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *    * Neither the names of the University of Washington nor Microsoft,
+ *      nor the names of its contributors may be used to endorse or promote
+ *      products derived from this software without specific prior written
+ *      permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Jacob O. Wobbrock OR Andrew D. Wilson
+ * OR Yang Li BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**/
+//
+// Point class
+//
+function Point(x, y) // constructor
+{
+	this.X = x;
+	this.Y = y;
+}
+//
+// Rectangle class
+//
+function Rectangle(x, y, width, height) // constructor
+{
+	this.X = x;
+	this.Y = y;
+	this.Width = width;
+	this.Height = height;
+}
+//
+// Unistroke class: a unistroke template
+//
+function Unistroke(name, points) // constructor
+{
+	this.Name = name;
+	this.Points = Resample(points, NumPoints);
+	var radians = IndicativeAngle(this.Points);
+	this.Points = RotateBy(this.Points, -radians);
+	this.Points = ScaleTo(this.Points, SquareSize);
+	this.Points = TranslateTo(this.Points, Origin);
+	this.Vector = Vectorize(this.Points); // for Protractor
+}
+//
+// Result class
+//
+function Result(name, score, ms) // constructor
+{
+	this.Name = name;
+	this.Score = score;
+	this.Time = ms;
+}
+//
+// DollarRecognizer constants
+//
+const NumUnistrokes = 2;
+const NumPoints = 64;
+const SquareSize = 250.0;
+const Origin = new Point(0,0);
+const Diagonal = Math.sqrt(SquareSize * SquareSize + SquareSize * SquareSize);
+const HalfDiagonal = 0.5 * Diagonal;
+const AngleRange = Deg2Rad(45.0);
+const AnglePrecision = Deg2Rad(2.0);
+const Phi = 0.5 * (-1.0 + Math.sqrt(5.0)); // Golden Ratio
+//
+// DollarRecognizer class
+//
+function DollarRecognizer() // constructor
+{
+	//
+	// one built-in unistroke per gesture type
+	//
+	this.Unistrokes = new Array(NumUnistrokes);
+	this.Unistrokes[0] = new Unistroke("circle", new Array(new Point(127,141),new Point(124,140),new Point(120,139),new Point(118,139),new Point(116,139),new Point(111,140),new Point(109,141),new Point(104,144),new Point(100,147),new Point(96,152),new Point(93,157),new Point(90,163),new Point(87,169),new Point(85,175),new Point(83,181),new Point(82,190),new Point(82,195),new Point(83,200),new Point(84,205),new Point(88,213),new Point(91,216),new Point(96,219),new Point(103,222),new Point(108,224),new Point(111,224),new Point(120,224),new Point(133,223),new Point(142,222),new Point(152,218),new Point(160,214),new Point(167,210),new Point(173,204),new Point(178,198),new Point(179,196),new Point(182,188),new Point(182,177),new Point(178,167),new Point(170,150),new Point(163,138),new Point(152,130),new Point(143,129),new Point(140,131),new Point(129,136),new Point(126,139)));
+    this.Unistrokes[1] = new Unistroke("caret", new Array (new Point(0,0),new Point(-1,1),new Point(-2,1),new Point(-3,0),new Point(-5,-2),new Point(-8,-6),new Point(-10,-10),new Point(-16,-19),new Point(-18,-23),new Point(-20,-28),new Point(-22,-32),new Point(-25,-37),new Point(-28,-43),new Point(-31,-48),new Point(-38,-59),new Point(-42,-65),new Point(-46,-73),new Point(-51,-80),new Point(-56,-85),new Point(-61,-92),new Point(-65,-98),new Point(-68,-102),new Point(-71,-105),new Point(-75,-108),new Point(-78,-112),new Point(-82,-115),new Point(-85,-119),new Point(-89,-122),new Point(-93,-124),new Point(-97,-127),new Point(-101,-130),new Point(-104,-132),new Point(-106,-134),new Point(-108,-135), new Point(-111,-138),new Point(-112,-139),new Point(-113,-140),new Point(-114,-141),new Point(-115,-142),new Point(-116,-142),new Point(-117,-141),new Point(-118,-140),new Point(-119,-139),new Point(-120,-138),new Point(-123,-135),new Point(-126,-130),new Point(-131,-125),new Point(-140,-112),new Point(-149,-97),new Point(-154,-89),new Point(-156,-86),new Point(-158,-81),new Point(-160,-77),new Point(-162,-73),new Point(-164,-69),new Point(-166,-65),new Point(-168,-60),new Point(-170,-55),new Point(-171,-51),new Point(-173,-46),new Point(-175,-42),new Point(-178,-37),new Point(-182,-32),new Point(-185,-27),new Point(-187,-22),new Point(-190,-17),new Point(-192,-12),new Point(-195,-7),new Point(-198,-2),new Point(-202,3),new Point(-204,6),new Point(-205,8),new Point(-206,9),new Point(-206,10),new Point(-207,10),new Point(-207,11))
+    );
+
+	//
+	// The $1 Gesture Recognizer API begins here -- 3 methods: Recognize(), AddGesture(), and DeleteUserGestures()
+	//
+	this.Recognize = function(points, useProtractor)
+	{
+		var t0 = Date.now();
+		points = Resample(points, NumPoints);
+		var radians = IndicativeAngle(points);
+		points = RotateBy(points, -radians);
+		points = ScaleTo(points, SquareSize);
+		points = TranslateTo(points, Origin);
+		var vector = Vectorize(points); // for Protractor
+
+		var b = +Infinity;
+		var u = -1;
+		for (var i = 0; i < this.Unistrokes.length; i++) // for each unistroke
+		{
+			var d;
+			if (useProtractor) // for Protractor
+				d = OptimalCosineDistance(this.Unistrokes[i].Vector, vector);
+			else // Golden Section Search (original $1)
+				d = DistanceAtBestAngle(points, this.Unistrokes[i], -AngleRange, +AngleRange, AnglePrecision);
+			if (d < b) {
+				b = d; // best (least) distance
+				u = i; // unistroke index
+			}
+		}
+		var t1 = Date.now();
+		return (u == -1) ? new Result("No match.", 0.0, t1-t0) : new Result(this.Unistrokes[u].Name, useProtractor ? 1.0 / b : 1.0 - b / HalfDiagonal, t1-t0);
+	}
+	this.AddGesture = function(name, points)
+	{
+		this.Unistrokes[this.Unistrokes.length] = new Unistroke(name, points); // append new unistroke
+		var num = 0;
+		for (var i = 0; i < this.Unistrokes.length; i++) {
+			if (this.Unistrokes[i].Name == name)
+				num++;
+		}
+		return num;
+	}
+	this.DeleteUserGestures = function()
+	{
+		this.Unistrokes.length = NumUnistrokes; // clear any beyond the original set
+		return NumUnistrokes;
+	}
+}
+//
+// Private helper functions from here on down
+//
+function Resample(points, n)
+{
+	var I = PathLength(points) / (n - 1); // interval length
+	var D = 0.0;
+	var newpoints = new Array(points[0]);
+	for (var i = 1; i < points.length; i++)
+	{
+		var d = Distance(points[i-1], points[i]);
+		if ((D + d) >= I)
+		{
+			var qx = points[i-1].X + ((I - D) / d) * (points[i].X - points[i-1].X);
+			var qy = points[i-1].Y + ((I - D) / d) * (points[i].Y - points[i-1].Y);
+			var q = new Point(qx, qy);
+			newpoints[newpoints.length] = q; // append new point 'q'
+			points.splice(i, 0, q); // insert 'q' at position i in points s.t. 'q' will be the next i
+			D = 0.0;
+		}
+		else D += d;
+	}
+	if (newpoints.length == n - 1) // somtimes we fall a rounding-error short of adding the last point, so add it if so
+		newpoints[newpoints.length] = new Point(points[points.length - 1].X, points[points.length - 1].Y);
+	return newpoints;
+}
+function IndicativeAngle(points)
+{
+	var c = Centroid(points);
+	return Math.atan2(c.Y - points[0].Y, c.X - points[0].X);
+}
+function RotateBy(points, radians) // rotates points around centroid
+{
+	var c = Centroid(points);
+	var cos = Math.cos(radians);
+	var sin = Math.sin(radians);
+	var newpoints = new Array();
+	for (var i = 0; i < points.length; i++) {
+		var qx = (points[i].X - c.X) * cos - (points[i].Y - c.Y) * sin + c.X
+		var qy = (points[i].X - c.X) * sin + (points[i].Y - c.Y) * cos + c.Y;
+		newpoints[newpoints.length] = new Point(qx, qy);
+	}
+	return newpoints;
+}
+function ScaleTo(points, size) // non-uniform scale; assumes 2D gestures (i.e., no lines)
+{
+	var B = BoundingBox(points);
+	var newpoints = new Array();
+	for (var i = 0; i < points.length; i++) {
+		var qx = points[i].X * (size / B.Width);
+		var qy = points[i].Y * (size / B.Height);
+		newpoints[newpoints.length] = new Point(qx, qy);
+	}
+	return newpoints;
+}
+function TranslateTo(points, pt) // translates points' centroid
+{
+	var c = Centroid(points);
+	var newpoints = new Array();
+	for (var i = 0; i < points.length; i++) {
+		var qx = points[i].X + pt.X - c.X;
+		var qy = points[i].Y + pt.Y - c.Y;
+		newpoints[newpoints.length] = new Point(qx, qy);
+	}
+	return newpoints;
+}
+
+function Vectorize(points) // for Protractor
+{
+	var sum = 0.0;
+	var vector = new Array();
+	for (var i = 0; i < points.length; i++) {
+		vector[vector.length] = points[i].X;
+		vector[vector.length] = points[i].Y;
+		sum += points[i].X * points[i].X + points[i].Y * points[i].Y;
+	}
+	var magnitude = Math.sqrt(sum);
+	for (var i = 0; i < vector.length; i++)
+		vector[i] /= magnitude;
+	return vector;
+}
+function OptimalCosineDistance(v1, v2) // for Protractor
+{
+	var a = 0.0;
+	var b = 0.0;
+	for (var i = 0; i < v1.length; i += 2) {
+		a += v1[i] * v2[i] + v1[i+1] * v2[i+1];
+		b += v1[i] * v2[i+1] - v1[i+1] * v2[i];
+	}
+	var angle = Math.atan(b / a);
+	return Math.acos(a * Math.cos(angle) + b * Math.sin(angle));
+}
+function DistanceAtBestAngle(points, T, a, b, threshold)
+{
+	var x1 = Phi * a + (1.0 - Phi) * b;
+	var f1 = DistanceAtAngle(points, T, x1);
+	var x2 = (1.0 - Phi) * a + Phi * b;
+	var f2 = DistanceAtAngle(points, T, x2);
+	while (Math.abs(b - a) > threshold)
+	{
+		if (f1 < f2) {
+			b = x2;
+			x2 = x1;
+			f2 = f1;
+			x1 = Phi * a + (1.0 - Phi) * b;
+			f1 = DistanceAtAngle(points, T, x1);
+		} else {
+			a = x1;
+			x1 = x2;
+			f1 = f2;
+			x2 = (1.0 - Phi) * a + Phi * b;
+			f2 = DistanceAtAngle(points, T, x2);
+		}
+	}
+	return Math.min(f1, f2);
+}
+function DistanceAtAngle(points, T, radians)
+{
+	var newpoints = RotateBy(points, radians);
+	return PathDistance(newpoints, T.Points);
+}
+function Centroid(points)
+{
+	var x = 0.0, y = 0.0;
+	for (var i = 0; i < points.length; i++) {
+		x += points[i].X;
+		y += points[i].Y;
+	}
+	x /= points.length;
+	y /= points.length;
+	return new Point(x, y);
+}
+function BoundingBox(points)
+{
+	var minX = +Infinity, maxX = -Infinity, minY = +Infinity, maxY = -Infinity;
+	for (var i = 0; i < points.length; i++) {
+		minX = Math.min(minX, points[i].X);
+		minY = Math.min(minY, points[i].Y);
+		maxX = Math.max(maxX, points[i].X);
+		maxY = Math.max(maxY, points[i].Y);
+	}
+	return new Rectangle(minX, minY, maxX - minX, maxY - minY);
+}
+function PathDistance(pts1, pts2)
+{
+	var d = 0.0;
+	for (var i = 0; i < pts1.length; i++) // assumes pts1.length == pts2.length
+		d += Distance(pts1[i], pts2[i]);
+	return d / pts1.length;
+}
+function PathLength(points)
+{
+	var d = 0.0;
+	for (var i = 1; i < points.length; i++)
+		d += Distance(points[i - 1], points[i]);
+	return d;
+}
+function Distance(p1, p2)
+{
+	var dx = p2.X - p1.X;
+	var dy = p2.Y - p1.Y;
+	return Math.sqrt(dx * dx + dy * dy);
+}
+function Deg2Rad(d) { return (d * Math.PI / 180.0); }
+
+
+rec = new DollarRecognizer()
+shape = rec.Recognize( new Array(new Point(0,0), new Point(-1,-204)))
+console.log(shape)
+
+rec = new DollarRecognizer()
+shape = rec.Recognize(new Array(new Point(0,0),new Point(2,-3),new Point(6,-12),new Point(26,-51),new Point(74,-133),new Point(83,-151),new Point(86,-154),new Point(87,-154),new Point(88,-154),new Point(94,-151),new Point(111,-143),new Point(137,-130),new Point(157,-116),new Point(175,-91),new Point(187,-68),new Point(195,-47),new Point(201,-32),new Point(204,-17),new Point(205,-7),new Point(206,-3),new Point(206,-1),new Point(206,0),new Point(206,1)))
+console.log(shape)
+
